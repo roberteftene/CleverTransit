d2a17d4b0505131dca10cc2702bf70361c7b8c05
roberteftene <roberteftene25@gmail.com>
2020-10-30 18:07:49 +0200

    firstSetup

---
+++ backend/server.js
@@ -0,0 +1,9 @@
+const { req, res } = require('express')
+const express = require('express')
+const app = express()
+
+app.use('/', express.static('../frontend'))
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+
+app.listen(8080)
