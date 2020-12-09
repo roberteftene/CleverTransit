2505ef6bcd410cc5d4fd88a6f62d828217a700d2
roberteftene <roberteftene25@gmail.com>
2020-12-09 17:34:39 +0200

    created TransportLine Model

--- backend/createData.js
+++ backend/createData.js
@@ -2,14 +2,20 @@ const model = require('./models');
 
 
 const data = [{name: "Bus"},{name:"Tram"}, {name:"Subway"},{name:"Train"},{name:"Popular"},{name:"My Reviews"}];
+const lineData = [{lineName: "M2",route:"A-B",transportMethodId: 8},{lineName: "101",route:"A-B",transportMethodId:5}];
 // This is for inserting data
+(async () => {
+    await model.TransportLine.bulkCreate(lineData);
+})()
+
+//This is for deleting tables
 // (async () => {
-//     await model.transportMethod.bulkCreate(data)
+//     await model.Line.drop()
 // })()
 
 //This is for deleting data
-(async () => {
-    await model.transportMethod.destroy({where: {
-        id:[1,2,3,4]
-    }});
-})()
+// (async () => {
+//     await model.transportMethod.destroy({where: {
+//         id:[1,2,3,4]
+//     }});
+// })()
