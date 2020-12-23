c5947eaa49f967d28fc0a81dcb4aabfb14ce1c29
roberteftene <roberteftene25@gmail.com>
2020-12-23 19:16:32 +0200

    get lines by mot

--- backend/controllers/TransportLineController.js
+++ backend/controllers/TransportLineController.js
@@ -1,4 +1,5 @@
 const transportLine = require('../models').TransportLine;
+const transportMethod = require('../models').transportMethod;
 
 const getTransportLines = async(req,res) => {
     let linesData;
@@ -10,6 +11,25 @@ const getTransportLines = async(req,res) => {
     return res.send(linesData);
 }
 
+const getTransportLineByTransportMethodId = async(req,res) => {
+    let transportMethodId;
+    try {
+        await transportMethod.findOne(
+            {
+                where: {id: req.params.id}
+            }
+        ).then((result) => transportMethodId = result.id);
+        await transportLine.findAll({
+            where: { transportMethodId: transportMethodId }
+        }).then(result => res.send(result));
+    } catch(err) {
+        return res.status(404).send({message: err.message})
+    }
+
+
+}
+
 module.exports = {
-    getTransportLines
+    getTransportLines,
+    getTransportLineByTransportMethodId
 }
--- backend/routes/index.js
+++ backend/routes/index.js
@@ -9,4 +9,5 @@ const reviewController = require('../controllers/ReviewController')
 router.get('/methods',methodOfTransportController.getAllMethods);
 router.get('/lines',transportLineController.getTransportLines);
 router.get('/reviews', reviewController.getAllReviews);
+router.get('/transport-method/:id/lines',transportLineController.getTransportLineByTransportMethodId);
 module.exports = router;
