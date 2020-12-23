0efe0c99e5397d39506f5210dc3baa0d0f9af84e
roberteftene <roberteftene25@gmail.com>
2020-12-23 16:56:33 +0200

    created review model

--- backend/models/index.js
+++ backend/models/index.js
@@ -3,8 +3,10 @@ let sequelize = require('./db.js');
 const path = require('path')
 const transportMethod = require(path.join(__dirname,'./MethodOfTransport.js'))(sequelize, Sequelize.DataTypes)
 const TransportLine = require(path.join(__dirname, './TransportLine'))(sequelize, Sequelize.DataTypes)
+const Review = require(path.join(__dirname,'./Review'))(sequelize, Sequelize.DataTypes)
 module.exports = {
     sequelize,
     transportMethod,
     TransportLine,
+    Review
 }
