b561babf655075521e60d40f0da4f65d0da1f729
roberteftene <roberteftene25@gmail.com>
2021-01-09 17:14:26 +0000

    Added navbar to existing pages

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,12 @@
 import './App.css';
 import Routes from  './Routes'
-
+import NavigationBar from './components/NavigationBar/NavigationBar';
 
 function App() {
   return (
     <div className="App">
-      <Routes/>
+        <NavigationBar />
+        <Routes/>
     </div>
   );
 }
