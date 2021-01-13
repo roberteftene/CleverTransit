dc5b9b004fc0a0ff6edc4ef9f1d64b96ae489859
Diana Crisan <crisanmonica18@stud.ase.ro>
2021-01-13 16:21:06 +0200

    added page not found functionality

--- frontend/src/Routes.js
+++ frontend/src/Routes.js
@@ -1,9 +1,10 @@
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import ReviewsPage from './containers/ReviewsPage/ReviewsPage';
 import ProfilePage from './containers/ProfilePage/ProfilePage';
 import HomePage from './containers/HomePage/HomePage';
 import LandingPage from './containers/LandingPage/LandingPage';
 import AboutUsPage from './containers/AboutUsPage/AboutUsPage';
+import NotFoundPage from './containers/NotFoundPage/NotFoundPage';
 
 
 export default function Routes() {
@@ -12,11 +13,14 @@ export default function Routes() {
             {/* Here will be all the paths
                 For more details visit: https://reactrouter.com/web/guides/quick-start
             */}
+            <Switch>
             <Route path="/reviews" exact component={ReviewsPage}></Route>
             <Route path="/profile" exact component={ProfilePage}></Route>
             <Route path="/home" exact component={HomePage}></Route>
             <Route path="/" exact component={LandingPage}></Route>
             <Route path="/aboutus" exact component={AboutUsPage}></Route>
+            <Route component={NotFoundPage}></Route>
+            </Switch>
             
         </Router>
     );
